deb61d565da99ee109cbbef2cf41d5c06e8d0de1
srinumeesala <srinusri76585@gmail.com>
2023-02-24 20:59:17 +0530

    qr code secret changed

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 import express from "express";
-import authorizeUser from "../middlewares/authorizeUser.js"
-import qr from "qrcode"
-import User from "../models/users.js"
+import authorizeUser from "../middlewares/authorizeUser.js";
+import qr from "qrcode";
+import User from "../models/users.js";
+import jwt from "jsonwebtoken";
 
 const userRoute = new express.Router()
 
@@ -27,10 +28,11 @@ const sendEmail = async (request, response) => {
 
 const sendQr = async (request, response) => {
     console.log("Accessed - Send QR API")
+    const qrEncodedKey = jwt.sign({ code: request.currentUser }, process.env.qrSecretCode);
     try {
         const user = await User.findOne({ email: request.currentUser })
-        const forQR = `${user.name}`
-        qr.toDataURL(request.currentUser, (err, src) => {
+        const forQR = `${qrEncodedKey}`
+        qr.toDataURL(forQR, (err, src) => {
             if (err) {
                 response.status(500)
                 response.send({ msg: "Couldnot get qr Image" })
