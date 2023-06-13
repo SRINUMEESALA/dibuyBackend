fa91e1a734848cf63525ccded1900dabb22abb1e
Srinu Meesala <srinusri76585@gmail.com>
2023-06-13 13:02:58 +0530

    small changes done

--- databaseConnections/AtlasDbConnection.js
+++ databaseConnections/AtlasDbConnection.js
@@ -1,14 +1,19 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const connectToRemoteDb = async () => {
-    try {
-        mongoose.set('strictQuery', false);
-        const remoteDbURI = process.env.remoteAtlasDbConnectionURI
-        await mongoose.connect(remoteDbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-        console.log("Remote Atlas Database Connection Successful!")
-    } catch (err) {
-        console.log("Remote Atlas Database Connection Failed!")
-    }
-}
+  try {
+    mongoose.set("strictQuery", false);
+    const remoteDbURI = process.env.remoteAtlasDbConnectionURI;
+    await mongoose.connect(remoteDbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Remote Atlas Database Connection Successful!");
+  } catch (err) {
+    console.log("Remote Atlas Database Connection Failed!", err);
+  }
+};
 
-export default connectToRemoteDb
+export default connectToRemoteDb;
--- middlewares/authorizeUser.js
+++ middlewares/authorizeUser.js
@@ -1,24 +1,25 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const authorizeUser = (request, response, next) => {
-    let jwtToken = request.headers["authorization"]
-    if (jwtToken === undefined) {
-        response.status(401)
-        response.send({ msg: "User is not authorized" })
-    } else {
-        jwtToken = jwtToken.split(" ")[1]
-        jwt.verify(jwtToken, process.env.secretCode, async (error, payload) => {
-            if (error) {
-                response.status(401)
-                response.send({ msg: "Jwt token not matched" })
-            } else {
-                request.currentUser = payload.UserEmail
-                console.log("User authorized")
-                next();
-            }
-        })
-    }
+  let jwtToken = request.headers["authorization"];
+  if (jwtToken === undefined) {
+    response.status(401);
+    response.send({ msg: "User is not authorized" });
+  } else {
+    jwtToken = jwtToken.split(" ")[1];
+    jwt.verify(jwtToken, process.env.secretCode, async (error, payload) => {
+      if (error) {
+        response.status(401);
+        response.send({ msg: "Jwt token not matched" });
+      } else {
+        request.currentUser = payload.UserEmail;
+        console.log("User authorized");
+        next();
+      }
+    });
+  }
+};
 
-}
-
-export default authorizeUser
+export default authorizeUser;
